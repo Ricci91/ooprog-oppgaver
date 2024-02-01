ea1c4eb849b296aa4a60e5a609b421ff84147695
Richard <froyland.richard@gmail.com>
2024-02-01 05:52:34 +0100

    oppg5 continued

--- oppg5/oppg5.cpp
+++ oppg5/oppg5.cpp
@@ -4,3 +4,29 @@
  * @file oppg5.cpp
  * @author Richard Froeyland, NTNU
 */
+
+#include <stdio.h>
+
+using namespace std;
+
+const int STRLEN = 40;  ///< Max.Tekstlengde
+const int MAXFORELESNINGER = 10; ///< Max.antall forelesninger
+
+struct Forelesning
+{
+    char emne[STRLEN/2],
+         foreleser[STRLEN],
+         sted[STRLEN/2];
+    int timeStart, minuttStart,
+        timeSlutt, minuttSlutt;
+};
+
+int main()
+{
+
+    
+
+
+
+    return 0;
+}
